5a88c47f2baabaabc8dea6f1a4808ece17f35ee8
blangus <burakkelebek@gmail.com>
2020-04-01 14:26:59 +0000

    added params to pipeline

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -1,4 +1,9 @@
 pipelineJob('pipeline') {
+    
+    parameters {
+        stringParam('myParameterName', 'my default stringParam value', 'my description')
+    }
+
     definition {
         cps {
             script(readFileFromWorkspace('pipeline/container-image-builder.groovy'))
